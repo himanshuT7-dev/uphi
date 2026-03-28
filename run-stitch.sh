2847e802310b203865cd6063702f45e82146e639
himanshuT7-dev <himanshutokekarr@gmail.com>
2026-03-28 20:19:41 +0530

    security: remove hardcoded secrets and move to environment variables

--- run-stitch.sh
+++ run-stitch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export STITCH_API_KEY="AQ.Ab8RN6K-B5PpLjTcsqgx6cBX4Q31TX8TpJ_j-2dQRWQcbCg96Q"
+export STITCH_API_KEY="${STITCH_API_KEY:-your_key_here}"
 export GOOGLE_CLOUD_PROJECT="uphi-college-project"
 exec ./node_modules/.bin/kof-stitch-mcp
